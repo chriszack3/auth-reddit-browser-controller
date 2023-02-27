584a48427cc7271bbb132d931d301d2b37e282e4
chriszack3 <czack@chrispc>
2023-02-27 17:42:03 -0500

    removed unused arg

--- app.mjs
+++ app.mjs
@@ -9,14 +9,14 @@ dotenv.config()
 const username = process.env.USER_NAME
 const password = process.env.PASS_WORD
 
-const run = async (sub) => {
+const run = async () => {
   const config = {
     headless: false,
     args: ["--disable-notifications"]
   }
   const browser = await puppeteer.launch(config);
   const page = await browser.newPage()
-  
+
   await page.goto(`https://www.reddit.com/login/`);
   // Type into search box.
   await page.type(`input[id="loginUsername"]`, username);
@@ -24,12 +24,12 @@ const run = async (sub) => {
   await page.click(`button[type="submit"]`)
   await page.waitForNavigation()
   await page.click(`i.icon-chat`)
-  
+
   await page.waitForSelector(`button[aria-label="Close"]`)
   await page.click(`button[aria-label="Close"]`)
   await page.waitForNetworkIdle()
   const msgThreads = await page.$$(`main#tooltip-container > div > div > div > div > div > a`)
-  
+
   let messagesArr = []
   for (let i = 0; i < msgThreads.length; i++) {
     await msgThreads[i].click()
@@ -45,39 +45,19 @@ const run = async (sub) => {
       return {
         threadMessages,
         usernames,
-        
+
       }
     })
     messagesArr.push(messages)
-    
+
   }
   const date = Date.now().toString()
-  
+
   try {
     fs.writeFileSync(`./logs/messageLogs/${date}.json`, JSON.stringify(messagesArr));
     // file written successfully
   } catch (err) {
     console.error(err);
   }
-  
-  //messages.forEach((thread) => {
-    // await thread.click()
-    // await page.waitForSelector(`#tooltip-container > div > div > div > div > div > span`)
-    // const threadContent = await page.$eval(`#tooltip-container > div > div > div > div > div > span`, (msg) => msg)
-    // const previousChats = await page.$$eval(`#tooltip-container > div > div > div > div > div`, (chats) => {
-    //   return chats
-    // })
-    //console.log(thread)
-  //document.querySelector("#tooltip-container > div > div > div > div> div > span > span > pre")
-  //})
-  // let i = 0
-  // for (i < messages.length - 1; i++;) {
-  //   await messages[i].click()
-  //   const previousChats = await page.$$(`#tooltip-container > div > div > div > div > div > span:nth-child(3) > span`, (chats) => chats.innerText)
-  //   console.log(previousChats)
-  //   i++
-  // }
-  
 }
-
-run('FootFetishTalks')
+run()
